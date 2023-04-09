2f8fceb2f92a79d5d271d78368369cf06e18487f
Viraka Prilitya <viraka@Virakas-MacBook-Air.local>
2023-04-09 21:20:28 +0700

    cros

--- server.js
+++ server.js
@@ -22,6 +22,15 @@ app.use("/api/counting", countingRoute);
 app.use("/api/device", deviceRoute);
 app.use("/api/history", historyRoute);
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.listen(port, () =>
   console.log(`App listening on port http://localhost:${port}!`)
 );
